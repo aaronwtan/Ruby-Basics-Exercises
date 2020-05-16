bb905edc399cdd186a85d46a0cbbd90db533b9b5
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-15 21:42:00 -0500

    Added Ex_6.rb

--- Variable Scope/Ex_6.rb
+++ Variable Scope/Ex_6.rb
@@ -1,8 +1,9 @@
 a = 7
 
 def my_value(b)
-  b = a + a
+  b = a + a # a is unavailable to my_value because methods have their own self-contained scope with its own local variables. 
+            # a here is undefined relative to the scope of my_value
 end
 
-my_value(a)
-puts a
+my_value(a) # calling my_value will produce an error because my_value tries to reference a which is outside of the method's scope
+puts a # this will never execute because of the preceding error invoked
