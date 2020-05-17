3986d2092319c18ee8fe1b02c7ad7cff2811b33e
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-17 00:58:00 -0500

    Added Methods exercises

---
+++ Methods/Ex_3.rb
@@ -0,0 +1,12 @@
+# Write two methods, one that returns the string "Hello" and one that returns the string "World". 
+# Then print both strings using #puts, combining them into one sentence.
+
+def say_hello
+  "Hello"
+end
+
+def say_world
+  "World"
+end
+
+puts say_hello + " " + say_world
