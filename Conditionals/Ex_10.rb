280d0ffdeddc415ea87d585c1e53b850b379b348
aaronwtan <18561712+aaronwtan@users.noreply.github.com>
2020-05-17 19:27:49 -0500

    Added Conditionals exercises

---
+++ Conditionals/Ex_10.rb
@@ -0,0 +1,7 @@
+# Reformat the following case statement so that it only takes up 5 lines.
+
+case stoplight = ['green', 'yellow', 'red'].sample
+when 'green'  then puts 'Go!'
+when 'yellow' then puts 'Slow down!'
+else               puts 'Stop!'
+end
